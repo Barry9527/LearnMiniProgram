3a4aae83c68ef2c27038447fa5d6a34ab0ec890b
Barry9527 <308747637@qq.com>
2019-11-24 23:30:33 +0800

    day01

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      name:'CoderWhy',
+      age:'18',
+      students:[
+        { id: 110, name: 'kobo', age: '30' },
+        { id: 111, name: 'barry', age: '25' },
+        { id: 112, name: 'roger', age: '20' },
+        { id: 113, name: 'allan', age: '24' }
+        ],
+        counter:0
   },
-
+  handleBtnClick(){
+    //1.错误做法：界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+    //2.this.setData()
+    this.setData({
+      counter:this.data.counter + 1
+    })
+  },
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter - 1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
